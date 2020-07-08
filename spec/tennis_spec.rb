5a5ce3a422a515a38f1aa661593822fc18f12253
hishambokhari <hish.b92@gmail.com>
2020-07-08 15:24:33 +0500

    Refactor

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -1,20 +1,19 @@
 require 'tennis'
 
 describe TennisScorer do
+  let(:tennis_scorer) { TennisScorer.new }
+  
   it 'can score a new game where no player has scored yet' do
-    tennis_scorer =  TennisScorer.new
     expect(tennis_scorer.score).to eq('Love-Love')
   end
 
   it 'can score a new game where player one has scored once' do
-    tennis_scorer =  TennisScorer.new
 
     tennis_scorer.won_point
     expect(tennis_scorer.score).to eq('Fifteen-Love')
   end
 
   it 'can score a new game where player one has scored twice' do
-    tennis_scorer =  TennisScorer.new
 
     tennis_scorer.won_point
     tennis_scorer.won_point
