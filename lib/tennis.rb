1041419345a21235b1eaccb9cc312906d5411ae3
hishambokhari <hish.b92@gmail.com>
2020-07-08 15:13:19 +0500

    Test 1 passing

---
+++ lib/tennis.rb
@@ -0,0 +1,5 @@
+class TennisScorer
+  def score
+    'Love-Love'
+  end
+end
--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -2,6 +2,10 @@ require 'tennis'
 
 describe TennisScorer do
   it 'can score a new game where no player has scored yet' do
+    tennis_score =  TennisScorer.new
+
+    score = tennis_score.score
+    
     expect(score).to eq('Love-Love')
   end
 end
