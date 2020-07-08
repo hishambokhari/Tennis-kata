f634daf42653b05b58fb55a06f4e0ea63443a9af
hishambokhari <hish.b92@gmail.com>
2020-07-08 15:10:33 +0500

    Add: test 1

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -1,35 +1,7 @@
-# frozen_string_literal: true
-
 require 'tennis'
 
-describe Tennis do
-  describe '#score' do
-    it 'can score a new game where no player has scored yet' do
-      expect(subject.score).to eq('Love-Love')
-    end
-
-    it 'player one scores a point' do
-      expect(subject.score_one).to eq('Fifteen-Love')
-    end
-
-    it 'player one scores two points' do
-      expect(subject.score_two).to eq('Thirty-Love')
-    end
-
-    it 'player one scores three points' do
-      expect(subject.score_three).to eq('Fourty-Love')
-    end
-
-    it 'player one wins if they score four points' do
-      expect(subject.score_four).to eq('Player One Wins!!')
-    end
-
-    it 'if both players have 40-40 the game is deuce' do
-      expect(subject.score_deuce).to eq('Deuce!!')
-    end
-
-    it 'player one wins if they score a point from deuce' do
-      expect(subject.adv_p1).to eq('Adv - Player one!')
-    end
+describe TennisScorer do
+  it 'can score a new game where no player has scored yet' do
+    expect(score).to eq('Love-Love')
   end
 end
